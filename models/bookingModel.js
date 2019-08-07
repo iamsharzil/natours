30578a818aab6542d594b41f483f504561f4a94f
Sharzil Shaikh <iamsharzil@gmail.com>
2019-08-07 18:05:15 +0530

    New Features added

---
+++ models/bookingModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+
+const bookingSchema = new mongoose.Schema(
+  {
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Tour',
+      required: [true, 'Booking must belong to a Tour']
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Booking must belong to a User']
+    },
+    price: {
+      type: Number,
+      required: [true, 'Booking must have a price']
+    },
+    paid: {
+      type: Boolean,
+      default: true
+    }
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
+  }
+);
+
+// Virtual populate
+bookingSchema.virtual('tours', {
+  ref: 'Tour',
+  foreignField: 'tour',
+  localField: '_id'
+});
+
+bookingSchema.pre('/^find/', function(next) {
+  this.populate('user').populate({
+    path: 'Tour',
+    select: 'name'
+  });
+
+  next();
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+
+module.exports = Booking;

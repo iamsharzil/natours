b2e2d8217dfd241f3e67cab46fb9628b2a3f0f46
Sharzil Shaikh <iamsharzil@gmail.com>
2019-08-08 16:16:14 +0530

    Improved Stripe implementation

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 app.post(
-  '/webook-checkout',
+  '/webhook-checkout',
   bodyParser.raw({ type: 'application/json' }),
   bookingController.webhookCheckout
 );
